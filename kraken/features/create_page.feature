f3de2582a35c6a099f03cf4ebd76bbf504760265
Miguel Fernando Padilla Espino <m.padillae@uniandes.edu.co>
2024-05-03 16:03:26 -0500

    kraken last version

---
+++ kraken/features/create_page.feature
@@ -0,0 +1,28 @@
+Feature: Crear páginas
+
+@user1 @web
+Scenario: Crear una página con título y cuerpos válidos y publicarla directamente.
+  Given I navigate to page "<GHOST_LOGIN_URL>"
+  And I wait for 5 seconds
+  And I fill the login form email with "<GHOST_USERNAME>"
+  And I wait for 1 seconds
+  And I fill the login form password with "<GHOST_PASSWORD>"
+  And I wait for 1 seconds
+  And I try to login
+  And I wait for 2 seconds
+  And I wait for the dashboard to be visible
+  When I navigate to page "<GHOST_PAGES_URL>"
+  And I wait for 2 seconds
+  And I start to create a new page
+  And I wait for 2 seconds
+  And I fill the page form title with "<VALID_PAGE_TITLE>"
+  And I wait for 1 seconds
+  And I fill the page form body with "<VALID_PAGE_BODY>"
+  And I wait for 1 seconds
+  And I click the Publish button
+  And I wait for 3 seconds
+  And I click the Final Review button
+  And I wait for 3 seconds
+  And I click the Publish Page button
+  And I wait for 3 seconds
+  Then I expect to see "<PUBLISH_PAGE_SUCCESS_MESSAGE>"
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,1 +1,58 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('I fill the login form email with {string}', async function (username) {
+    let element = await this.driver.$('#identification');
+    return await element.setValue(username);
+});
+
+Given('I fill the login form password with {string}', async function (password) {
+    let element = await this.driver.$('#password');
+    return await element.setValue(password);
+});
+
+Given('I try to login', async function() {
+    let element = await this.driver.$('#ember5');
+    return await element.click();
+})
+
+Given('I wait for the dashboard to be visible', async function() {
+    let element = await this.driver.url()
+    console.log(element);
+    return await element.click();
+})
+
+When('I start to create a new page', async function() {
+    let element = await this.driver.$('a[href="#/editor/page/"]');
+    return await element.click();
+})
+
+When('I fill the page form title with {string}', async function (title) {
+    let element = await this.driver.$('.gh-editor-title.ember-text-area.gh-input.ember-view');
+    return await element.setValue(title);
+});
+
+When('I fill the page form body with {string}', async function (body) {
+    let element = await this.driver.$('p[data-koenig-dnd-droppable="true"]');
+    return await element.setValue(body);
+});
+
+When('I click the Publish button', async function() {
+    let element = await this.driver.$('button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger');
+    return await element.click();
+})
+
+When('I click the Final Review button', async function() {
+    let element = await this.driver.$('button.gh-btn.gh-btn-black.gh-btn-large');
+    return await element.click();
+})
+
+When('I click the Publish Page button', async function() {
+    let element = await this.driver.$('button.gh-btn.gh-btn-large.gh-btn-pulse.ember-view');
+    return await element.click();
+})
+
+Then('I expect to see {string}', async function (message) {
+    let element = await this.driver.$('span.green');
+    console.log(element)
+    return await element.text() == message;
+});
