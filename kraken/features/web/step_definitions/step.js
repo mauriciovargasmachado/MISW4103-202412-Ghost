8bca4cd877ed6dde715970e2cdc3dd27c305a29c
Miguel Fernando Padilla Espino <m.padillae@uniandes.edu.co>
2024-05-03 16:23:30 -0500

    first scenario kraken

--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,11 +1,12 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
-Given('I fill the login form email with {string}', async function (username) {
+Given('I fill the login form email with {kraken-string}', async function (username) {
     let element = await this.driver.$('#identification');
+    console.log(username);
     return await element.setValue(username);
 });
 
-Given('I fill the login form password with {string}', async function (password) {
+Given('I fill the login form password with {kraken-string}', async function (password) {
     let element = await this.driver.$('#password');
     return await element.setValue(password);
 });
@@ -16,9 +17,8 @@ Given('I try to login', async function() {
 })
 
 Given('I wait for the dashboard to be visible', async function() {
-    let element = await this.driver.url()
-    console.log(element);
-    return await element.click();
+    let element = await this.driver.$('.gh-canvas-title');
+    return await element.getValue() == "Dashboard";
 })
 
 When('I start to create a new page', async function() {
@@ -26,12 +26,15 @@ When('I start to create a new page', async function() {
     return await element.click();
 })
 
-When('I fill the page form title with {string}', async function (title) {
+When('I fill the page form title with {kraken-string}', async function (title) {
     let element = await this.driver.$('.gh-editor-title.ember-text-area.gh-input.ember-view');
     return await element.setValue(title);
 });
 
-When('I fill the page form body with {string}', async function (body) {
+When('I fill the page form body with {kraken-string}', async function (body) {
+    let e = this.driver.$('.kg-prose');
+    e.click();
+    await new Promise(r => setTimeout(r, 1000));
     let element = await this.driver.$('p[data-koenig-dnd-droppable="true"]');
     return await element.setValue(body);
 });
@@ -51,8 +54,7 @@ When('I click the Publish Page button', async function() {
     return await element.click();
 })
 
-Then('I expect to see {string}', async function (message) {
+Then('I expect to see {kraken-string}', async function (message) {
     let element = await this.driver.$('span.green');
-    console.log(element)
-    return await element.text() == message;
+    return await element.getValue() == message;
 });
