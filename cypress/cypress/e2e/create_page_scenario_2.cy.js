99e5cdecfbb3a9d5109d7e149a1485b490a693fe
Miguel Fernando Padilla Espino <m.padillae@uniandes.edu.co>
2024-05-08 12:42:20 -0500

    adding screenshots to create page scenarios

--- cypress/cypress/e2e/create_page_scenario_2.cy.js
+++ cypress/cypress/e2e/create_page_scenario_2.cy.js
@@ -3,14 +3,18 @@ describe('Funcionalidad: Crear páginas', () => {
     // Go to Ghost login screen.
     cy.visit(Cypress.env('GHOST_LOGIN_URL'))
     cy.wait(5000)
+    cy.screenshot('create_page_scenario_2/1', {overwrite: true})
 
     // Fill the login form with <GHOST_USERNAME> and <GHOST_PASSWORD>	
     cy.get('input.gh-input.email').type(Cypress.env('GHOST_USERNAME'))
+    cy.screenshot('create_page_scenario_2/2', {overwrite: true})
     cy.get('input.gh-input.password').type(Cypress.env('GHOST_PASSWORD'))
+    cy.screenshot('create_page_scenario_2/3', {overwrite: true})
 
     // Try to login
     cy.get('button.login.gh-btn.gh-btn-login.gh-btn-block.gh-btn-icon.ember-view').click()
     cy.wait(2000)
+    cy.screenshot('create_page_scenario_2/4', {overwrite: true})
 
     // Wait for the dashboard to be visible
     cy.url().should('eq', Cypress.env('GHOST_DASHBOARD_URL'))
@@ -18,27 +22,37 @@ describe('Funcionalidad: Crear páginas', () => {
   it('Crear una página con título y descripción válidos y programar su publicación.', ()=>{
     // Try to create a new page
     cy.visit(Cypress.env('GHOST_PAGES_URL'))
+    cy.screenshot('create_page_scenario_2/5', {overwrite: true})
     cy.get('a[href="#/editor/page/"]').eq(0).click()
     cy.wait(5000)
+    cy.screenshot('create_page_scenario_2/6', {overwrite: true})
 
     // Fill the page form with <VALID_PAGE_TITLE> and <VALID_PAGE_BODY>
     cy.get('.gh-editor-title.ember-text-area.gh-input.ember-view').type(Cypress.env('VALID_PAGE_TITLE'))
+    cy.screenshot('create_page_scenario_2/7', {overwrite: true})
     cy.wait(1000)
     cy.get('.kg-prose').click()
     cy.wait(1000)
+    cy.screenshot('create_page_scenario_2/8', {overwrite: true})
     cy.get('p[data-koenig-dnd-droppable="true"]').type(Cypress.env('VALID_PAGE_BODY'));
     cy.wait(1000)
+    cy.screenshot('create_page_scenario_2/9', {overwrite: true})
 
     // Try to publish the page
     cy.get('button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger').eq(0).click()
+    cy.screenshot('create_page_scenario_2/10', {overwrite: true})
     cy.wait(3000)
     cy.get('button.gh-publish-setting-title').click()
+    cy.screenshot('create_page_scenario_2/11', {overwrite: true})
     cy.wait(3000)
     cy.get('div[data-test-radio="schedule"]').click({ force: true })
+    cy.screenshot('create_page_scenario_2/12', {overwrite: true})
     cy.wait(3000)
     cy.get('button.gh-btn.gh-btn-black.gh-btn-large').click()
+    cy.screenshot('create_page_scenario_2/13', {overwrite: true})
     cy.wait(3000)
     cy.get('button.gh-btn.gh-btn-large.gh-btn-pulse.ember-view').click()
+    cy.screenshot('create_page_scenario_2/14', {overwrite: true})
     cy.wait(5000)
 
     // Expect to see <SCHEDULE_PAGE_SUCCESS_MESSAGE>
--- cypress/cypress/e2e/create_page_scenario_4.cy.js
+++ cypress/cypress/e2e/create_page_scenario_4.cy.js
@@ -3,14 +3,18 @@ describe('Funcionalidad: Crear páginas', () => {
     // Go to Ghost login screen.
     cy.visit(Cypress.env('GHOST_LOGIN_URL'))
     cy.wait(5000)
+    cy.screenshot('create_page_scenario_4/1', {overwrite: true})
 
     // Fill the login form with <GHOST_USERNAME> and <GHOST_PASSWORD>	
     cy.get('input.gh-input.email').type(Cypress.env('GHOST_USERNAME'))
+    cy.screenshot('create_page_scenario_4/2', {overwrite: true})
     cy.get('input.gh-input.password').type(Cypress.env('GHOST_PASSWORD'))
+    cy.screenshot('create_page_scenario_4/3', {overwrite: true})
 
     // Try to login
     cy.get('button.login.gh-btn.gh-btn-login.gh-btn-block.gh-btn-icon.ember-view').click()
     cy.wait(2000)
+    cy.screenshot('create_page_scenario_4/4', {overwrite: true})
 
     // Wait for the dashboard to be visible
     cy.url().should('eq', Cypress.env('GHOST_DASHBOARD_URL'))
@@ -18,22 +22,30 @@ describe('Funcionalidad: Crear páginas', () => {
   it('Crear una página con título pero sin cuerpo y publicarla directamente.', ()=>{
     // Try to create a new page
     cy.visit(Cypress.env('GHOST_PAGES_URL'))
+    cy.screenshot('create_page_scenario_4/5', {overwrite: true})
     cy.get('a[href="#/editor/page/"]').eq(0).click()
     cy.wait(5000)
+    cy.screenshot('create_page_scenario_4/6', {overwrite: true})
 
     // Fill the page form with <EMPTY_PAGE_TITLE> and <VALID_PAGE_BODY>
     cy.get('.gh-editor-title.ember-text-area.gh-input.ember-view').invoke('val', Cypress.env('EMPTY_PAGE_TITLE'));
+    cy.screenshot('create_page_scenario_4/7', {overwrite: true})
     cy.wait(1000)
     cy.get('.kg-prose').click()
     cy.wait(1000)
+    cy.screenshot('create_page_scenario_4/8', {overwrite: true})
     cy.get('p[data-koenig-dnd-droppable="true"]').type(Cypress.env('VALID_PAGE_BODY'))
     cy.wait(1000)
+    cy.screenshot('create_page_scenario_4/9', {overwrite: true})
 
     // Try to publish the page
     cy.get('button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger').eq(0).click()
+    cy.screenshot('create_page_scenario_4/10', {overwrite: true})
     cy.wait(3000)
     cy.get('button.gh-btn.gh-btn-black.gh-btn-large').click()
+    cy.screenshot('create_page_scenario_4/11', {overwrite: true})
     cy.wait(3000)
+    cy.screenshot('create_page_scenario_4/12', {overwrite: true})
     cy.get('button.gh-btn.gh-btn-large.gh-btn-pulse.ember-view').click()
     cy.wait(5000)
 
