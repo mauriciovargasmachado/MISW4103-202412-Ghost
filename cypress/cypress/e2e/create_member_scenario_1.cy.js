09f4e0011251e972f025f20322bb6f625f4db52e
JohannPaezU <js.paezc1@uniandes.edu.co>
2024-05-09 23:51:48 -0500

    Cypress screenshots for member create scenarios

--- cypress/cypress/e2e/create_member_scenario_1.cy.js
+++ cypress/cypress/e2e/create_member_scenario_1.cy.js
@@ -5,14 +5,18 @@ describe('Funcionalidad: Crear miembro', () => {
     // Go to Ghost login screen.
     cy.visit(Cypress.env('GHOST_LOGIN_URL'));
     cy.wait(5000);
+    cy.screenshot('create_member_scenario_1/1', {overwrite: true})
 
     // Fill the login form with <GHOST_USERNAME> and <GHOST_PASSWORD>	
     cy.get('input.gh-input.email').type(Cypress.env('GHOST_USERNAME'));
+    cy.screenshot('create_member_scenario_1/2', {overwrite: true})
     cy.get('input.gh-input.password').type(Cypress.env('GHOST_PASSWORD'));
+    cy.screenshot('create_member_scenario_1/3', {overwrite: true})
 
     // Try to login
     cy.get('button.login.gh-btn.gh-btn-login.gh-btn-block.gh-btn-icon.ember-view').click();    
     cy.wait(2000);
+    cy.screenshot('create_member_scenario_1/4', {overwrite: true})
 
     // Wait for the dashboard to be visible
     cy.url().should('eq', Cypress.env('GHOST_DASHBOARD_URL'));
@@ -22,21 +26,27 @@ describe('Funcionalidad: Crear miembro', () => {
     cy.visit(Cypress.env('GHOST_MEMBERS_URL'));
     cy.wait(3000);
     cy.url().should('eq', Cypress.env('GHOST_MEMBERS_URL'))
+    cy.screenshot('create_member_scenario_1/5', {overwrite: true})
     cy.get('a[href="#/members/new/"]').eq(0).click();
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_1/6', {overwrite: true})
 
     // Fill the member information with random data
     var email = faker.internet.email();
     cy.get('input[name="name"]').type(faker.person.fullName());
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_1/7', {overwrite: true})
     cy.get('input[name="email"]').type(email);
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_1/8', {overwrite: true})
     cy.get('textarea[name="note"]').type(faker.string.alphanumeric(150));
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_1/9', {overwrite: true})
 
     // Try to save the new member
     cy.get('button.gh-btn.gh-btn-primary.gh-btn-icon.ember-view').click();
-    cy.wait(3000);
+    cy.wait(2000);
+    cy.screenshot('create_member_scenario_1/10', {overwrite: true})
 
     // Expect that the creation message contains <MEMBER_SUCCESS_MESSAGE>
     cy.get('div.gh-member-details-attribution p').then(($p)=>{
@@ -45,8 +55,9 @@ describe('Funcionalidad: Crear miembro', () => {
 
     // Check the new member in the members list
     cy.visit(Cypress.env('GHOST_MEMBERS_URL'));
-    cy.wait(3000);
+    cy.wait(3000);    
     cy.url().should('eq', Cypress.env('GHOST_MEMBERS_URL'))    
+    cy.screenshot('create_member_scenario_1/11', {overwrite: true})
     var emails = [];
     cy.get('div table.gh-list tbody tr').each(($tr) => {
         cy.wrap($tr).find('a').then(($a) => {
--- cypress/cypress/e2e/create_member_scenario_2.cy.js
+++ cypress/cypress/e2e/create_member_scenario_2.cy.js
@@ -5,14 +5,18 @@ describe('Funcionalidad: Crear miembro', () => {
     // Go to Ghost login screen.
     cy.visit(Cypress.env('GHOST_LOGIN_URL'));
     cy.wait(5000);
+    cy.screenshot('create_member_scenario_2/1', {overwrite: true})
 
     // Fill the login form with <GHOST_USERNAME> and <GHOST_PASSWORD>	
     cy.get('input.gh-input.email').type(Cypress.env('GHOST_USERNAME'));
+    cy.screenshot('create_member_scenario_2/2', {overwrite: true})
     cy.get('input.gh-input.password').type(Cypress.env('GHOST_PASSWORD'));
+    cy.screenshot('create_member_scenario_2/3', {overwrite: true})
 
     // Try to login
     cy.get('button.login.gh-btn.gh-btn-login.gh-btn-block.gh-btn-icon.ember-view').click();    
     cy.wait(2000);
+    cy.screenshot('create_member_scenario_2/4', {overwrite: true})
 
     // Wait for the dashboard to be visible
     cy.url().should('eq', Cypress.env('GHOST_DASHBOARD_URL'));
@@ -20,21 +24,26 @@ describe('Funcionalidad: Crear miembro', () => {
   it('Crear un miembro con una dirección de correo electrónico ya existente', ()=> {
     // Try to create a new member
     cy.visit(Cypress.env('GHOST_MEMBERS_URL'));
-    cy.wait(3000);
+    cy.wait(3000);    
     cy.url().should('eq', Cypress.env('GHOST_MEMBERS_URL'))
+    cy.screenshot('create_member_scenario_2/5', {overwrite: true})
     cy.get('a[href="#/members/new/"]').eq(0).click();
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_2/6', {overwrite: true})
 
     // Fill the member information with random data
     var email = faker.internet.email();
     cy.get('input[name="name"]').type(faker.person.fullName());
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_2/7', {overwrite: true})
     cy.get('input[name="email"]').type(email);
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_2/8', {overwrite: true})
 
     // Try to save the new member
     cy.get('button.gh-btn.gh-btn-primary.gh-btn-icon.ember-view').click();
-    cy.wait(3000);
+    cy.wait(2000);
+    cy.screenshot('create_member_scenario_2/9', {overwrite: true})
 
     // Expect that the creation message contains <MEMBER_SUCCESS_MESSAGE>
     cy.get('div.gh-member-details-attribution p').then(($p)=>{
@@ -45,6 +54,7 @@ describe('Funcionalidad: Crear miembro', () => {
     cy.visit(Cypress.env('GHOST_MEMBERS_URL'));
     cy.wait(3000);
     cy.url().should('eq', Cypress.env('GHOST_MEMBERS_URL'))
+    cy.screenshot('create_member_scenario_2/10', {overwrite: true})
     var emails = [];
     cy.get('div table.gh-list tbody tr').each(($tr) => {
         cy.wrap($tr).find('a').then(($a) => {
@@ -60,12 +70,15 @@ describe('Funcionalidad: Crear miembro', () => {
     // Try to create a new member with the same email
     cy.get('a[href="#/members/new/"]').eq(0).click();
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_2/11', {overwrite: true})
 
     // Fill the member information with random data and the same email
     cy.get('input[name="name"]').type(faker.person.fullName());
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_2/12', {overwrite: true})
     cy.get('input[name="email"]').type(email);
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_2/13', {overwrite: true})
 
     // Ignore uncaught exceptions
     Cypress.on('uncaught:exception', (err, runnable, promise) => {
@@ -75,6 +88,7 @@ describe('Funcionalidad: Crear miembro', () => {
     // Try to save the new member
     cy.get('button.gh-btn.gh-btn-primary.gh-btn-icon.ember-view').eq(0).click();
     cy.wait(3000);
+    cy.screenshot('create_member_scenario_2/14', {overwrite: true})
     
     // Expect that the creation message contains <EXISTING_MEMBER_ERROR_MESSAGE>
     cy.get('div.form-group.max-width.error p.response').then(($svg)=>{
