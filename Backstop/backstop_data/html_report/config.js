46fe61f7be9d0300e68230bb3e794a07fa34e40c
Mauricio Vargas <mauriciovargasmachado@gmail.com>
2024-05-11 18:41:41 -0400

    Creacion escenario 1 backstop

---
+++ Backstop/backstop_data/html_report/config.js
@@ -0,0 +1,295 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_1_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_1_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_1_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/1.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/1.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.42602237654321,
+          "misMatchPercentage": "3.43",
+          "analysisTime": 90
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_2_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_2_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_2_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 2",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/2.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/2.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.80174575617284,
+          "misMatchPercentage": "3.80",
+          "analysisTime": 82
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_3_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_3_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_3_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 3",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/3.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/3.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.661458333333333,
+          "misMatchPercentage": "3.66",
+          "analysisTime": 81
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_4_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_4_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_4_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 4",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/4.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/4.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 2.1183449074074074,
+          "misMatchPercentage": "2.12",
+          "analysisTime": 93
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_5_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_5_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_5_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 5",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/5.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/5.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 4.415461033950617,
+          "misMatchPercentage": "4.42",
+          "analysisTime": 74
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_6_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_6_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_6_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 6",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/6.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/6.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 4.337770061728395,
+          "misMatchPercentage": "4.34",
+          "analysisTime": 50
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_7_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_7_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_7_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 7",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/7.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/7.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 14.11646412037037,
+          "misMatchPercentage": "14.12",
+          "analysisTime": 62
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-183251\\failed_diff_backstop_default_Backstop_test_create_draft_scenario_1_image_7_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_8_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_8_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_8_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 8",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/8.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/8.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 14.4858699845679,
+          "misMatchPercentage": "14.49",
+          "analysisTime": 53
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-183251\\failed_diff_backstop_default_Backstop_test_create_draft_scenario_1_image_8_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_9_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_9_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_9_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 9",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/9.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/9.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 14.619020061728394,
+          "misMatchPercentage": "14.62",
+          "analysisTime": 71
+        },
+        "diffImage": "..\\bitmaps_test\\20240511-183251\\failed_diff_backstop_default_Backstop_test_create_draft_scenario_1_image_9_0_document_0_default.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_10_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_10_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_10_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 10",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/10.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/10.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 4.3490065586419755,
+          "misMatchPercentage": "4.35",
+          "analysisTime": 60
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Backstop_test_create_draft_scenario_1_image_11_0_document_0_default.png",
+        "test": "..\\bitmaps_test\\20240511-183251\\backstop_default_Backstop_test_create_draft_scenario_1_image_11_0_document_0_default.png",
+        "selector": "document",
+        "fileName": "backstop_default_Backstop_test_create_draft_scenario_1_image_11_0_document_0_default.png",
+        "label": "Backstop test create draft scenario_1 image 11",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 8,
+        "url": "../kraken/screenshots/create_draft_scenario_1/11.png",
+        "referenceUrl": "../kraken/screenshots/create_draft_scenario_1_old/11.png",
+        "expect": 0,
+        "viewportLabel": "default",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 4.3490065586419755,
+          "misMatchPercentage": "4.35",
+          "analysisTime": 55
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
